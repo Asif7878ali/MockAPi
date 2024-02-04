1ab50046d436df754f79d5fd45ad8c2328024606
AsifRiseofPower <aloasif31@gmail.com>
2024-02-04 18:15:08 +0530

    cors

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const jsonServer = require('json-server')
 const index = jsonServer.create()
 const router = jsonServer.router("datas.json")
 const middle = jsonServer.defaults()
+const cors = require('cors');
+
+// Use CORS middleware with options if needed
+server.use(cors());
+
 const port = process.env.PORT  || 3000
 
 index.use(middle)
